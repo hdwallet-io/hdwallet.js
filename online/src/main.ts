2eeea0ba7f1aa85aee7dc8b033fd442697485af3
meherett <meherett.batu@gmail.com>
2025-07-09 12:15:57 +0300

    Add: Google tag & splash-screen

--- online/src/main.ts
+++ online/src/main.ts
@@ -3,5 +3,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).then(() => {
+  const splashScreen: HTMLElement | null = document.getElementById('splash-screen');
+  const appRoot: HTMLElement | null = document.getElementById('app-root');
+
+  if (splashScreen) {
+    splashScreen.style.opacity = '0'; // Fade out splash screen
+    setTimeout(() => {
+      splashScreen.remove(); // Remove splash screen element from DOM
+
+      if (appRoot) {
+        appRoot.classList.add('visible'); // Add 'visible' class to app-root
+      }
+    }, 500); // Match the CSS transition duration (500ms)
+  }
+}).catch((err) => console.error(err));
