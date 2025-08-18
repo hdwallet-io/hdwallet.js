3095eea4316b94b95eb6b2cec314994a269e7909
meherett <meherett.batu@gmail.com>
2025-08-18 14:04:54 +0300

    Add: cryptocurrency symbols

---
+++ src/symbols.ts
@@ -0,0 +1,424 @@
+// SPDX-License-Identifier: MIT
+
+// Adcoin
+export const ACC: string = 'ACC';
+// Akash-Network
+export const AKT: string = 'AKT';
+// Algorand
+export const ALGO: string = 'ALGO';
+// Anon
+export const ANON: string = 'ANON';
+// Aptos
+export const APT: string = 'APT';
+// Arbitrum
+export const ARB: string = 'ARB';
+// Argoneum
+export const AGM: string = 'AGM';
+// Artax
+export const XAX: string = 'XAX';
+// Aryacoin
+export const AYA: string = 'AYA';
+// Asiacoin
+export const AC: string = 'AC';
+// Auroracoin
+export const AUR: string = 'AUR';
+// Avalanche
+export const AVAX: string = 'AVAX';
+// Avian
+export const AVN: string = 'AVN';
+// Axe
+export const AXE: string = 'AXE';
+// Axelar
+export const AXL: string = 'AXL';
+// Band-Protocol
+export const BAND: string = 'BAND';
+// Base
+export const BASE: string = 'BASE';
+// Bata
+export const BTA: string = 'BTA';
+// Beetle Coin
+export const BEET: string = 'BEET';
+// Bela Coin
+export const BELA: string = 'BELA';
+// Binance
+export const BNB: string = 'BNB';
+// Bit Cloud
+export const BTDX: string = 'BTDX';
+// Bitcoin
+export const BTC: string = 'BTC';
+// Bitcoin Atom
+export const BCA: string = 'BCA';
+// Bitcoin-Cash
+export const BCH: string = 'BCH';
+// Bitcoin-Cash-SLP
+export const SLP: string = 'SLP';
+// Bitcoin Gold
+export const BTG: string = 'BTG';
+// Bitcoin-Green
+export const BITG: string = 'BITG';
+// Bitcoin Plus
+export const XBC: string = 'XBC';
+// Bitcoin-Private
+export const BTCP: string = 'BTCP';
+// Bitcoin SV
+export const BSV: string = 'BSV';
+// BitcoinZ
+export const BTCZ: string = 'BTCZ';
+// Bitcore
+export const BTX: string = 'BTX';
+// Bit Send
+export const BSD: string = 'BSD';
+// Blackcoin
+export const BLK: string = 'BLK';
+// Blocknode
+export const BND: string = 'BND';
+// Block Stamp
+export const BST: string = 'BST';
+// Bolivarcoin
+export const BOLI: string = 'BOLI';
+// Brit Coin
+export const BRIT: string = 'BRIT';
+// Canada eCoin
+export const CDN: string = 'CDN';
+// Cannacoin
+export const CCN: string = 'CCN';
+// Cardano
+export const ADA: string = 'ADA';
+// Celo
+export const CELO: string = 'CELO';
+// Chihuahua
+export const HUA: string = 'HUA';
+// Clams
+export const CLAM: string = 'CLAM';
+// Club Coin
+export const CLUB: string = 'CLUB';
+// Compcoin
+export const CMP: string = 'CMP';
+// Cosmos
+export const ATOM: string = 'ATOM';
+// CPU Chain
+export const CPU: string = 'CPU';
+// Crane Pay
+export const CRP: string = 'CRP';
+// Crave
+export const CRAVE: string = 'CRAVE';
+// Dash
+export const DASH: string = 'DASH';
+// DeepOnion
+export const ONION: string = 'ONION';
+// Defcoin
+export const DFC: string = 'DFC';
+// Denarius
+export const DNR: string = 'DNR';
+// Diamond
+export const DMD: string = 'DMD';
+// Digi Byte
+export const DGB: string = 'DGB';
+// Digitalcoin
+export const DGC: string = 'DGC';
+// Divi
+export const DIVI: string = 'DIVI';
+// Dogecoin
+export const DOGE: string = 'DOGE';
+// dYdX
+export const DYDX: string = 'DYDX';
+// eCash
+export const XEC: string = 'XEC';
+// E-coin
+export const ECN: string = 'ECN';
+// EDR Coin
+export const EDRC: string = 'EDRC';
+// e-Gulden
+export const EFL: string = 'EFL';
+// Einsteinium
+export const EMC2: string = 'EMC2';
+// Elastos
+export const ELA: string = 'ELA';
+// Energi
+export const NRG: string = 'NRG';
+// EOS
+export const EOS: string = 'EOS';
+// Ergo
+export const ERG: string = 'ERG';
+// Ethereum
+export const ETH: string = 'ETH';
+// Europe Coin
+export const ERC: string = 'ERC';
+// Evrmore
+export const EVR: string = 'EVR';
+// Exclusive Coin
+export const EXCL: string = 'EXCL';
+// Fantom
+export const FTM: string = 'FTM';
+// Feathercoin
+export const FTC: string = 'FTC';
+// Fetch.ai
+export const FET: string = 'FET';
+// Filecoin
+export const FIL: string = 'FIL';
+// Firo
+export const FIRO: string = 'FIRO';
+// Firstcoin
+export const FRST: string = 'FRST';
+// FIX
+export const FIX: string = 'FIX';
+// Flashcoin
+export const FLASH: string = 'FLASH';
+// Flux
+export const FLUX: string = 'FLUX';
+// Foxdcoin
+export const FOXD: string = 'FOXD';
+// Fuji Coin
+export const FJC: string = 'FJC';
+// Game Credits
+export const GAME: string = 'GAME';
+// GCR Coin
+export const GCR: string = 'GCR';
+// Go Byte
+export const GBX: string = 'GBX';
+// Gridcoin
+export const GRC: string = 'GRC';
+// Groestl Coin
+export const GRS: string = 'GRS';
+// Gulden
+export const NLG: string = 'NLG';
+// Harmony
+export const ONE: string = 'ONE';
+// Helleniccoin
+export const HNC: string = 'HNC';
+// Hempcoin
+export const THC: string = 'THC';
+// Horizen
+export const ZEN: string = 'ZEN';
+// Huobi Token
+export const HT: string = 'HT';
+// Hush
+export const HUSH: string = 'HUSH';
+// ICON
+export const ICX: string = 'ICX';
+// Injective
+export const INJ: string = 'INJ';
+// InsaneCoin
+export const INSN: string = 'INSN';
+// Internet Of People
+export const IOP: string = 'IOP';
+// IRISnet
+export const IRIS: string = 'IRIS';
+// IX Coin
+export const IXC: string = 'IXC';
+// Jumbucks
+export const JBS: string = 'JBS';
+// Kava
+export const KAVA: string = 'KAVA';
+// Kobocoin
+export const KOBO: string = 'KOBO';
+// Komodo
+export const KMD: string = 'KMD';
+// Landcoin
+export const LDCN: string = 'LDCN';
+// LBRY Credits
+export const LBC: string = 'LBC';
+// Linx
+export const LINX: string = 'LINX';
+// Litecoin
+export const LTC: string = 'LTC';
+// Litecoin Cash
+export const LCC: string = 'LCC';
+// LitecoinZ
+export const LTZ: string = 'LTZ';
+// Lkrcoin
+export const LKR: string = 'LKR';
+// Lynx
+export const LYNX: string = 'LYNX';
+// Mazacoin
+export const MZC: string = 'MZC';
+// Megacoin
+export const MEC: string = 'MEC';
+// Metis
+export const METIS: string = 'METIS';
+// Minexcoin
+export const MNX: string = 'MNX';
+// Monacoin
+export const MONA: string = 'MONA';
+// Monero
+export const XMR: string = 'XMR';
+// Monk
+export const MONK: string = 'MONK';
+// MultiversX
+export const EGLD: string = 'EGLD';
+// Myriadcoin
+export const XMY: string = 'XMY';
+// Namecoin
+export const NMC: string = 'NMC';
+// Nano
+export const XNO: string = 'XNO';
+// Navcoin
+export const NAV: string = 'NAV';
+// Near
+export const NEAR: string = 'NEAR';
+// Neblio
+export const NEBL: string = 'NEBL';
+// Neo
+export const NEO: string = 'NEO';
+// Neoscoin
+export const NEOS: string = 'NEOS';
+// Neurocoin
+export const NRO: string = 'NRO';
+// Neutron
+export const NTRN: string = 'NTRN';
+// New York Coin
+export const NYC: string = 'NYC';
+// Nine-Chronicles
+export const NCG: string = 'NCG';
+// NIX
+export const NIX: string = 'NIX';
+// Novacoin
+export const NVC: string = 'NVC';
+// NuBits
+export const NBT: string = 'NBT';
+// NuShares
+export const NSR: string = 'NSR';
+// OK Cash
+export const OK: string = 'OK';
+// OKT-Chain
+export const OKT: string = 'OKT';
+// Omni
+export const OMNI: string = 'OMNI';
+// Onix
+export const ONX: string = 'ONX';
+// Ontology
+export const ONT: string = 'ONT';
+// Optimism
+export const OP: string = 'OP';
+// Osmosis
+export const OSMO: string = 'OSMO';
+// Particl
+export const PART: string = 'PART';
+// Peercoin
+export const PPC: string = 'PPC';
+// Pesobit
+export const PSB: string = 'PSB';
+// Phore
+export const PHR: string = 'PHR';
+// Pi-Network
+export const PI: string = 'PI';
+// Pinkcoin
+export const PINK: string = 'PINK';
+// Pivx
+export const PIVX: string = 'PIVX';
+// Polygon
+export const MATIC: string = 'MATIC';
+// PoSW Coin
+export const POSW: string = 'POSW';
+// Potcoin
+export const POT: string = 'POT';
+// Project Coin
+export const PRJ: string = 'PRJ';
+// Putincoin
+export const PUT: string = 'PUT';
+// Qtum
+export const QTUM: string = 'QTUM';
+// Rapids
+export const RPD: string = 'RPD';
+// Ravencoin
+export const RVN: string = 'RVN';
+// Reddcoin
+export const RDD: string = 'RDD';
+// Ripple
+export const XRP: string = 'XRP';
+// Ritocoin
+export const RITO: string = 'RITO';
+// RSK
+export const RBTC: string = 'RBTC';
+// Rubycoin
+export const RBY: string = 'RBY';
+// Safecoin
+export const SAFE: string = 'SAFE';
+// Saluscoin
+export const SLS: string = 'SLS';
+// Scribe
+export const SCRIBE: string = 'SCRIBE';
+// Secret
+export const SCRT: string = 'SCRT';
+// Shadow Cash
+export const SDC: string = 'SDC';
+// Shentu
+export const CTK: string = 'CTK';
+// Slimcoin
+export const SLM: string = 'SLM';
+// Smileycoin
+export const SMLY: string = 'SMLY';
+// Solana
+export const SOL: string = 'SOL';
+// Solarcoin
+export const SLR: string = 'SLR';
+// Stafi
+export const FIS: string = 'FIS';
+// Stash
+export const STASH: string = 'STASH';
+// Stellar
+export const XLM: string = 'XLM';
+// Stratis
+export const STRAT: string = 'STRAT';
+// Sugarchain
+export const SUGAR: string = 'SUGAR';
+// Sui
+export const SUI: string = 'SUI';
+// Syscoin
+export const SYS: string = 'SYS';
+// Terra
+export const LUNA: string = 'LUNA';
+// Tezos
+export const XTZ: string = 'XTZ';
+// Theta
+export const THETA: string = 'THETA';
+// Thought AI
+export const THT: string = 'THT';
+// TOA Coin
+export const TOA: string = 'TOA';
+// Tron
+export const TRX: string = 'TRX';
+// TWINS
+export const TWINS: string = 'TWINS';
+// Ultimate Secure Cash
+export const USC: string = 'USC';
+// Unobtanium
+export const UNO: string = 'UNO';
+// Vcash
+export const VC: string = 'VC';
+// VeChain
+export const VET: string = 'VET';
+// Verge
+export const XVG: string = 'XVG';
+// Vertcoin
+export const VTC: string = 'VTC';
+// Viacoin
+export const VIA: string = 'VIA';
+// Vivo
+export const VIVO: string = 'VIVO';
+// Voxels
+export const VOX: string = 'VOX';
+// Virtual Cash
+export const VASH: string = 'VASH';
+// Wagerr
+export const WGR: string = 'WGR';
+// Whitecoin
+export const XWC: string = 'XWC';
+// Wincoin
+export const WC: string = 'WC';
+// XinFin
+export const XDC: string = 'XDC';
+// XUEZ
+export const XUEZ: string = 'XUEZ';
+// Ycash
+export const YEC: string = 'YEC';
+// Zcash
+export const ZEC: string = 'ZEC';
+// ZClassic
+export const ZCL: string = 'ZCL';
+// Zetacoin
+export const ZET: string = 'ZET';
+// Zilliqa
+export const ZIL: string = 'ZIL';
+// ZooBC
+export const ZBC: string = 'ZBC';
