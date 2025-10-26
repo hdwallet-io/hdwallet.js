9ac3baba887d0d0a2457acee84b55f741225b3a3
meherett <meherett.batu@gmail.com>
2025-10-26 11:41:54 +0300

    Add: test Coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
-// jest.config.js
+// SPDX-License-Identifier: MIT
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': ["ts-jest", { tsconfig: "tsconfig.tests.json" }],
-    '^.+\\.jsx?$': ["ts-jest", { tsconfig: "tsconfig.tests.json" }],
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.tests.json' }],
+    '^.+\\.jsx?$': ['ts-jest', { tsconfig: 'tsconfig.tests.json' }],
   },
-
   transformIgnorePatterns: [
     '/node_modules/(?!(?:@stablelib|@noble|cbor2|@cto\\.af)/)'
   ],
@@ -15,5 +15,15 @@ module.exports = {
   ],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
-  }
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts'
+  ],
+  coverageReporters: [
+    'text', 'lcov', 'html'
+  ]
 };
